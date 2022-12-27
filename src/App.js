52b1d273c187905ca39e6a543ce9d4bcd4bf3e06
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2022-12-27 17:29:14 -0300

    feat: :lipstick: feito o Header (components e estilos)
    tambem criado rotas iniciais de login e criação de conta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from './components/Home';
+import Login from './components/Login/Login';
+
 function App() {
   return (
-    <div className="App">
-      Teste
+    <div>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import styles from './Header.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.css";
+import { ReactComponent as DogzSvg } from "../../Assets/dogs.svg";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      Header
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogz - Home">
+          <DogzSvg/>
+        </Link>
+        <Link to="/login" className={styles.login}>Login / Criar</Link>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
