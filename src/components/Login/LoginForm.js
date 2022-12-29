4eaf7a69878bc48872176b96256b7249e23f6a46
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2022-12-29 18:00:46 -0300

    refactor: :recycle: atualizado fetchs para async/wait
    e criado arquivo separado apenas para os endereços do fetch

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import Input from '../Forms/Input';
-import Button from '../Forms/Button';
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
+import { GET_USER, TOKEN_POST } from "../../api";
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-  
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) getUser(token);
+  }, [])
+
+  async function getUser(token) {
+    const { url, options } = GET_USER(token);
+
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const response = await fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      })
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
       const json = await response.json;
-      console.log(json);
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
     }
   }
 
@@ -28,9 +42,9 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" {...username}/>
+        <Input label="Usuário" type="text" name="username" {...username} />
 
-        <Input label="Senha" type="password" name="password" {...password}/>
+        <Input label="Senha" type="password" name="password" {...password} />
 
         <Button>Logar</Button>
       </form>
