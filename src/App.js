de4594318a5f79704aff5daac978f7686f5af985
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-01-16 20:26:55 -0300

    style: :lipstick: estilização do footer
    gg

--- src/App.js
+++ src/App.js
@@ -15,26 +15,28 @@ import Page404 from "./components/Page404";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
-            <Route
-              path="/conta/*"
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/foto/:id" element={<Photo />} />
-            <Route path="/perfil/:user" element={<UserProfile />} />
-            <Route path="/*" element={<Page404 />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login/*" element={<Login />} />
+              <Route
+                path="/conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/foto/:id" element={<Photo />} />
+              <Route path="/perfil/:user" element={<UserProfile />} />
+              <Route path="/*" element={<Page404 />} />
+            </Routes>
+          </main>
 
           <Footer />
         </UserStorage>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import {ReactComponent as DogzSvg} from '../../Assets/dogs-footer.svg';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Footer
+      <DogzSvg/>
+      <p>Doguinhoz. Alguns direitos reservados.</p>
     </footer>
   )
 }
