95ac3a5850e9cdb946f52e944467ae415d0c42cd
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-01-17 16:20:20 -0300

    refactor: :zap: adc lazy e suspense
    para mais rapido carregamento e performance

--- src/components/User/UserStats/UserStats.js
+++ src/components/User/UserStats/UserStats.js
@@ -1,33 +1,35 @@
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import useFetch from "../../../Hooks/useFetch";
 import { GET_STATS } from "../../../api";
 import Loading from "../../Loading";
 import { Erro } from "../../Erro";
 import Head from "../../Head";
-import Graphs from "./Graphs";
+const Graphs = lazy(() => import("./Graphs"));
 
 const UserStats = () => {
   const { data, loading, erro, request } = useFetch();
 
   useEffect(() => {
-    async function getData() {
-      const { url, options } = GET_STATS();
-      await request(url, options);
+    if (data) {
+      async function getData() {
+        const { url, options } = GET_STATS();
+        await request(url, options);
+      }
+      getData();
     }
-    getData();
-  }, [request]);
+  }, [data, request]);
 
   if (erro) return <Erro erro={erro} />;
   if (loading) return <Loading />;
   if (data)
     return (
-      <div>
+      <Suspense fallback={<div></div>}>
         <Head
           title="Estatísticas"
           description="Página de estatísticas do usuário."
         />
-        <Graphs data={data}/>
-      </div>
+        <Graphs data={data} />
+      </Suspense>
     );
   else return null;
 };
